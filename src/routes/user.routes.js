d625a7de08fd669f66e4741122839336ecece789
Akash Rajput <rajputakash7911@gmail.com>
2025-11-20 20:46:30 +0530

    integrate access token and refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 const router = Router();
 
@@ -12,5 +16,10 @@ router.route("/register").post(
 );
 
 router.route("/register").post(registerUser);
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -20,6 +20,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     // file has been uploaded
     console.log("File has been uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload got operation got failed
